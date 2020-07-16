37f00229c88714502959fd39048633f4d88b1df7
Sawka155 <vasilev9000@mail.ru>
2020-07-16 14:50:41 +0300

    fix

---
+++ js/scipt.js
@@ -0,0 +1,15 @@
+	var button = $(".block-1__btn__o");
+	var burger = $(".block-1__btn__o-hover");
+	console.log(burger);
+
+
+	button.onclick = function()
+	{if(burger.style.display = "flex"){
+			{jQuery(function($){				
+				    $(document).mouseup(function (e){ // отслеживаем событие клика по веб-документу	        
+						   burger.style.display = "none"   	
+				    });
+				});
+			};
+		}
+	};
