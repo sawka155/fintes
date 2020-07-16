14a5f723ebfa2bea5b8a45e9ec469b47d2b7e58d
Sawka155 <vasilev9000@mail.ru>
2020-07-16 15:45:26 +0300

    Готов первы и второй блок + написаны скрипты

--- js/scipt.js
+++ js/scipt.js
@@ -1,3 +1,17 @@
+$("#menu").mouseup(function(e){
+	$(".navbar_menu__active").css({'opacity':'1', 'display':'flex', 'transition':'1s'})
+	$("*").mousedown(function(e){
+	$(".navbar_menu__active").css({'opacity':'0', 'display':'none', 'transition':'1s'})});		
+});
+//header: menu
+$("#comm").mouseup(function(e){
+	$(".communication__active").css({'opacity':'1', 'display':'flex', 'transition':'1s'})
+	$("*").mousedown(function(e){
+	$(".communication__active").css({'opacity':'0', 'display':'none', 'transition':'1s'})});		
+});
+//header: call
+
+
 $("#vk").mousemove(function(e){
 	$(this).css({'filter':'invert(100%)', 'transition':'0.6s', 'cursor':'pointer'})
 	$(this).mouseout(function(e){
@@ -8,7 +22,7 @@ $("#vk").mousemove(function(e){
 		$(this).css({'filter':'none'})});
 	});
 });
-
+//block-1: btn1 and btn2
 
 
 
@@ -17,7 +31,7 @@ $(".block-2-map_block").mousemove(function(e){
 	$(this).mouseout(function(e){
 	$(this).css({'filter':'none'})});		
 });
-
+//block-2: btn1-4
 	
 
 
